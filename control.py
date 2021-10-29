48bb65d74851817b323f162ea9aa7846900e4e44
MonicaCortinas <mcortinas@unavarra.es>
2021-10-29 08:22:30 +0200

    nuevo ejercicio if else

---
+++ control.py
@@ -0,0 +1,21 @@
+z = 4
+#%%
+
+if z % 2 == 0: # TRUE
+    print("checking " + str(z))
+    print("z is even")
+else : 
+    print("z is odd")
+
+# %%
+z = 5
+if z % 2 == 0: # TRUE
+    print("checking " + str(z))
+    print("z is even")
+elif z % 3 == 0 :
+    print("checking " + str(z))
+    print("z is divisible by 3")
+else : 
+    print("checking " + str(z))
+    print("z is neither divisible by 2 nor by 3")
+# %%
